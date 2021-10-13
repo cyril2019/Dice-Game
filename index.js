59fee58ad71c6bc569d4c62b1bcc0457790ad8e7
Cyril Kunjumon <cyril 2019@iiitkottayam.ac.in>
2021-10-13 21:55:02 +0530

    Dice game 1.0

---
+++ index.js
@@ -0,0 +1,55 @@
+let num_selected = -1;
+let score = 0;
+const dice = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
+function select(num) {
+    document.getElementById("guess").innerHTML = num;
+    num_selected = num;
+}
+
+function updateScore() {
+    document.getElementById("score").innerHTML = score;
+}
+
+function changeDice() {
+    let x = Math.ceil(Math.random() * 6);
+    document.getElementById("imgDice").src = dice[x - 1];
+    document.getElementById("btext").innerHTML =
+        "Next round will start in a few seconds!";
+    if (num_selected == -1) {
+        document.getElementById("restext").style.color = "yellow";
+        document.getElementById(
+            "restext"
+        ).innerHTML = `You didnt make a guess.`;
+    } else if (x == num_selected) {
+        document.getElementById("restext").style.color = "lightgreen";
+        document.getElementById("restext").innerHTML = `Your Guess was right.`;
+        score += 10;
+        updateScore();
+    } else {
+        document.getElementById("restext").style.color = "red";
+        document.getElementById("restext").innerHTML = `Your guess was wrong.`;
+    }
+    setTimeout(game, 2000);
+    num_selected = -1;
+    document.getElementById("guess").innerHTML = "No guess";
+}
+
+function timer() {
+    var num = 5;
+    function countdown() {
+        if (num == -1) {
+            changeDice();
+            return;
+        }
+        document.getElementById(
+            "btext"
+        ).innerHTML = `The dice will change in ${num} seconds.`;
+        num--;
+        setTimeout(countdown, 1000);
+    }
+    countdown();
+}
+function game() {
+    document.getElementById("restext").innerHTML = ``;
+    setTimeout(timer, 1000);
+}
